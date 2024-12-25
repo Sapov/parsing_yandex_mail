545fb2eff9d708b75260f8d5cc3ff1868679f336
Sapov <sapov@mail.ru>
2024-12-25 15:15:22 +0300

    add models.py

--- mysite/parse_mail/models.py
+++ mysite/parse_mail/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class BaseMail(models.Model):
+    email = models.CharField(max_length=64, verbose_name='email', unique=True)
+    name = models.CharField(max_length=128, verbose_name='Имена в почте')
+    email_in_body = models.CharField(max_length=255, verbose_name='Почтовые имена в теле письма')
+    phones = models.CharField(max_length=255, verbose_name='Телефоны в письме')
+
+    def __str__(self):
+        return self.email
